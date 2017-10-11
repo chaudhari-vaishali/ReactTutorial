f6bb742f024070aec73ed6d5683451c683803cee
Sachin Warke <sachin.warke@teradata.com>
2017-10-11 10:57:55 +0530

    First commit

---
+++ src/UserEntryForm.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import './App.css';
+
+class UserEntryForm extends React.Component {
+  submitDetails (){
+    var errorMsg = '';
+    if(document.getElementById("first_name").value === '' ||
+    document.getElementById("last_name").value === '' ||
+    document.getElementById("phone").value === '' ||
+    document.getElementById("email").value === '' ||
+    document.getElementById("address").value === '')
+    {
+      errorMsg = "Please enter all fields.";
+      console.log("if",errorMsg);
+      // this.props.bottomUpData(errorMsg);
+    }
+    else {
+      errorMsg = '';
+      var dataIn = {
+                    first_name : document.getElementById("first_name").value, last_name : document.getElementById("last_name").value,
+                    phone : document.getElementById("phone").value, email : document.getElementById("email").value,
+                    address : document.getElementById("address").value
+                  }
+        // console.log("Test data ++ ", dataIn );
+        this.props.bottomUpData(dataIn);
+    }
+    
+  }
+
+  resetForm () {
+    document.getElementById("first_name").value = '';
+    document.getElementById("last_name").value = '';
+    document.getElementById("phone").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("address").value = '';
+  }
+
+  render() {    
+    return(    
+       <div className = "form-group  text-left">
+       <h4>{this.errorMsg}</h4>      
+        <label className="col-12 text-left">First Name: </label>
+        <input className="form-control" type="text" id="first_name" name="first name" placeholder="First Name" value={this.props.first_name} required/><br />
+        <label className="col-12 text-left">Last Name:</label>
+        <input className="form-control" type="text"  id="last_name" name="last name" placeholder="Last Name"  value = {this.props.last_name} required/><br />
+        <label className="col-12 text-left">Phone Number:</label>
+        <input className="form-control" type="number"  id="phone" name="phone number" placeholder="Phone" value = {this.props.phone} required/><br />
+        <label className="col-12 text-left">Email:</label>
+        <input className="form-control" type="email"  id="email" name="email" placeholder="Email" value = {this.props.email} required/><br />
+        <label className="col-12 text-left">Address:</label>
+        <textarea className="form-control" id="address" name="Address" placeholder="Address" value = {this.props.address}/><br />
+        <button className="btn btn-primary btn-md col-md-4" onClick={()=>this.submitDetails()}>Submit</button>
+        <button className="btn btn-warning btn-md col-md-4 margin-left-1" onClick={()=>this.resetForm()}>Reset</button>
+      </div>
+    )
+  }
+}
+
+export default UserEntryForm;
